be863e3478ef1492064de52ed10a8d3568bb3afc
Silje Møller <Silje.Moller@cegal.com>
2022-05-09 17:34:38 +0200

    add connection string static and getscreentext function

---
+++ ConnectionString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace memo_backend_net
+{
+    public static class ConnectionString
+    {
+        public static string DBConnectionString = 
+            "Server=tcp:twitch-plays.database.windows.net,1433;Initial Catalog=memo;Persist Security Info=False;User ID=memo;Password=Plays_987123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+    }
+}
--- GetScreenText.cs
+++ GetScreenText.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -7,45 +9,42 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using memo_backend_net;
 
-namespace Company.Function
+namespace memo_backend_net
 {
-    public static class SetScreenText
+    public static class GetScreenText
     {
-        [FunctionName("SetScreenText")]
+        [FunctionName("GetScreenText")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string setText = req.Query["setText"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            setText = setText ?? data?.setText;
-
-            var resultDict = new Dictionary<string, string>();
-            resultDict["message"] = "hei";
-            resultDict["setText"] = setText;
 
             var connectionString = ConnectionString.DBConnectionString;
+            var resultDict = new Dictionary<string, string>();
 
+            var s = "";
             using (var conn = new SqlConnection(connectionString))
             {
                 conn.Open();
-                var text = $"UPDATE test SET value ='{setText}' WHERE id=1";
-
+                var text = "SELECT * FROM test";
                 using (SqlCommand cmd = new SqlCommand(text, conn))
                 {
-                    // Execute the command and log the # rows affected.
-                    var rows = await cmd.ExecuteNonQueryAsync();
-                    log.LogInformation($"{rows} rows were updated");
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            s += String.Format("{0}, {1}", reader[0], reader[1]);
+                            log.LogInformation($"{s}");
+                        }
+                    }
                 }
             }
+
+            resultDict["sqlResult"] = s;
 
             var resultJson = JsonConvert.SerializeObject(resultDict);
 
