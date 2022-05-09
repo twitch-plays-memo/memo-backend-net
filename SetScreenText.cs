63f85659cd6f82a224ccec060bfc4899f2f084fe
Silje Møller <Silje.Moller@cegal.com>
2022-05-09 17:03:18 +0200

    try with sql reader

--- SetScreenText.cs
+++ SetScreenText.cs
@@ -34,16 +34,25 @@ namespace Company.Function
             var connectionString =
                 "Server=tcp:twitch-plays.database.windows.net,1433;Initial Catalog=memo;Persist Security Info=False;User ID=memo;Password=Plays_987123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
-            await using (var conn = new SqlConnection(connectionString))
+            using (var conn = new SqlConnection(connectionString))
             {
                 conn.Open();
                 var text = "SELECT * FROM test";
-                await using (SqlCommand cmd = new SqlCommand(text, conn))
+                using (SqlCommand cmd = new SqlCommand(text, conn))
                 {
-                    // Execute the command and log the # rows affected.
-                    var result = await cmd.ExecuteNonQueryAsync();
-                    log.LogInformation($"{result}");
-                    resultDict["sqlResult"] = $"{result}";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var s = String.Format("{0}, {1}", reader[0], reader[1]);
+                            log.LogInformation($"{s}");
+                            resultDict["sqlResult"] += s;
+                        }
+                    }
+                    //// Execute the command and log the # rows affected.
+                    //var result = cmd.ExecuteNonQuery();
+                    //log.LogInformation($"{result}");
                 }
             }
 
